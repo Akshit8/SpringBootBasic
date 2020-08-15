ed15846dd27fdb421afce440e041f47c7add13ae
akshit <akshitsadana@gmail.com>
2020-08-16 01:18:54 +0530

    CRUD on topic service2

---
+++ src/main/java/org/example/topic/TopicService.java
@@ -0,0 +1,43 @@
+package org.example.topic;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TopicService {
+
+    private List<Topic> topics = new ArrayList<>(Arrays.asList (
+                new Topic("node", "NodeJS", "javascript http framework"),
+                new Topic("spring", "spring boot", "spring framework description"),
+                new Topic("golang", "go micro", "go rest api controller")
+        ));
+
+    public List<Topic> getAllTopics() {
+        return topics;
+    }
+
+    public Topic getTopic(String id) {
+        return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+    }
+
+    public void addTopic(Topic topic) {
+        topics.add(topic);
+    }
+
+    public void updateTopic(String id, Topic topic) {
+        for (int i = 0; i < topics.size(); i++) {
+            Topic t = topics.get(i);
+            if(t.getId().equals(id)) {
+                topics.set(i, topic);
+                return;
+            }
+        }
+    }
+
+    public void deleteTopic(String id) {
+        topics.removeIf(t -> t.getId().equals(id));
+    }
+}
